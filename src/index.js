5b8e4f363665f2f0235c3e2abdaed8642f8805dd
Manh Hung <hungdm1211@gmail.com>
2019-11-14 16:59:29 +0700

    allow query production api from localhost

--- src/index.js
+++ src/index.js
@@ -1,14 +1,22 @@
-import { GraphQLServer } from 'graphql-yoga';
-import prisma from './prisma';
-import Query from './resolvers/Query';
-import Mutation from './resolvers/Mutation';
+import {GraphQLServer} from 'graphql-yoga'
+import prisma from './prisma'
+import Query from './resolvers/Query'
+import Mutation from './resolvers/Mutation'
 
 const server = new GraphQLServer({
     typeDefs: './src/schema.graphql',
-    resolvers: { Query, Mutation },
-    context: { prisma }
-});
+    resolvers: {Query, Mutation},
+    context: {prisma},
+})
 
-server.start(() => {
-    console.log('Server is up!');
-});
+server.start(
+    {
+        cors: {
+            origin: ['http://localhost:3000'],
+        },
+    },
+    () => {
+        // your frontend url.
+        console.log('Server is up!')
+    }
+)
