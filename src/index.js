e59e4a0e9d18220358a11a9258a5109c326cac52
Khoa <nanhkhoa460@gmaml.com>
2019-11-24 18:32:53 +0700

    semicolons

--- src/index.js
+++ src/index.js
@@ -1,19 +1,19 @@
-import {GraphQLServer} from 'graphql-yoga'
-import prisma from './prisma'
-import Query from './resolvers/Query'
-import Mutation from './resolvers/Mutation'
+import { GraphQLServer } from 'graphql-yoga';
+import prisma from './prisma';
+import Query from './resolvers/Query';
+import Mutation from './resolvers/Mutation';
 
 const server = new GraphQLServer({
     typeDefs: './src/schema.graphql',
-    resolvers: {Query, Mutation},
-    context: {prisma},
-})
+    resolvers: { Query, Mutation },
+    context: { prisma },
+});
 
 const opts = {
     port: 4000,
     cors: '*',
-}
+};
 
 server.start(opts, () => {
     console.log(`Server is running on http://localhost:${opts.port}`)
-})
+});
