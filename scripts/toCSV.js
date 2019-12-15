24951fa196530c26a7f40745e43ea6e894df76c8
Manh Hung <hungdm1211@gmail.com>
2019-12-15 10:31:46 +0700

    update csv files

--- scripts/toCSV.js
+++ scripts/toCSV.js
@@ -5,30 +5,22 @@ let students = [
         name: 'Đỗ Mạnh Hùng',
         studentID: '17020783',
         email: '17020783@vnu.edu.vn',
-        courses: 'EET3027 1, CTE3009 1, INT3307 1, EET3023 1, EET3001 1, INT3121 1, INT3506 1, EMA3021 1, INT2203 1',
-        nonEligibleCourses: 'INT3506 1, EMA3021 1, INT2203 1',
+        courses: 'EET3027 1, CTE3009 1,EET3023 1,INT3307 1,EET3001 1,INT3121 1, INT3506 1,EMA3021 1,EPN2001 1',
+        nonEligibleCourses: 'INT3506 1,EMA3021 1,EPN2001 1',
     },
     {
         name: 'Nguyễn Anh Khoa',
         studentID: '17020009',
         email: '17020009@vnu.edu.vn',
-        courses: 'INT3506 1, EMA3021 1, EPN2001 1, AGT 2002 1, INT3313 1, INT2203 1, EET3001 1, CTE2001 1, INT3414 1',
-        nonEligibleCourses: 'EET3001 1, CTE2001 1, INT3414 1',
+        courses: 'AGT 2002 1, INT3313 1, INT2203 1, INT3402 1, INT3414 1, ELT2028 1, PHY1100 20, EMA2037 1, CTE2001 1',
+        nonEligibleCourses: 'PHY1100 20, EMA2037 1, CTE2001 1',
     },
     {
         name: 'Nguyễn Thị Xuân Dung',
         studentID: '17020638',
         email: '17020638@vnu.edu.vn',
-        courses: 'INT3402 1, INT3414 1, ELT2028 1, PHY1100 20, EMA2037 1, CTE2001 1, INT3506 1, EMA3021 1, EPN2001 1',
-        nonEligibleCourses: 'INT3506 1, EMA3021 1, EPN2001 1',
-    },
-    {
-        name: 'Nguyễn Văn A',
-        studentID: '17021234',
-        email: '17021234@vnu.edu.vn',
-        courses:
-            'EMA3014 1, EMA3015 1, CTE2004 1, INT3201 1, EET3027 1, CTE3009 1, INT3307 1, EET3023 1, EET3001 1, INT3121 1',
-        nonEligibleCourses: 'EET3027 1, CTE3009 1, INT3307 1, EET3023 1, EET3001 1, INT3121 1',
+        courses: 'EMA3014 1, EMA3015 1, CTE2004 1, INT3201 1, EET2003 1, INT2207 1, INT3206 20, EMA 2030 1, EMA2033 1',
+        nonEligibleCourses: 'INT3206 20, EMA 2030 1, EMA2033 1',
     },
 ]
 
